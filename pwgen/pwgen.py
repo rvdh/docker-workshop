3e4304f3556ca82a8ce6d23d37ca2b7c2b595bdf
Rick van den Hof <r.vandenhof@ultimaker.com>
2019-03-29 11:20:36 +0100

    Save password to /volume

--- pwgen/pwgen.py
+++ pwgen/pwgen.py
@@ -2,4 +2,8 @@
 from xkcdpass import xkcd_password as xp
 
 password = xp.generate_xkcdpassword(xp.generate_wordlist())
+
+with open("/volume/password.txt", "w") as fh:
+  fh.write(password)
+
 print(password)
