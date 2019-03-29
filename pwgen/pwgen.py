9534f759a72330b7568a26eab581ed07f887f6c2
Rick van den Hof <r.vandenhof@ultimaker.com>
2019-03-29 16:03:58 +0100

    Read password back from file

--- pwgen/pwgen.py
+++ pwgen/pwgen.py
@@ -3,7 +3,11 @@ from xkcdpass import xkcd_password as xp
 
 password = xp.generate_xkcdpassword(xp.generate_wordlist())
 
-with open("/volume/password.txt", "w") as fh:
-  fh.write(password)
+try:
+  with open("/volume/password.txt", "r") as fh:
+    password = fh.readlines()[0]
+except:
+  with open("/volume/password.txt", "w") as fh:
+    fh.write(password)
 
 print(password)
